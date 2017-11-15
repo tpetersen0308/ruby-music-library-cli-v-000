263b574648b19bc8a5c719fb324ca796c332c33e
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-15 21:19:05 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,11 +41,11 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     user_artist = gets.chomp
 
-    if Artist.all.any?{ |artist| artist.name == user_artist }
+    #if Artist.all.any?{ |artist| artist.name == user_artist }
       Artist.all.detect{ |artist| artist.name == user_artist }.songs.sort_by{ |song| song.name }.each.with_index do |song, index|
         puts "#{index + 1}. #{song.name} - #{song.genre.name}"
       end
-    end
+    #end
   end
 
 end
