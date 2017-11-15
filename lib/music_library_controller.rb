b1435336e244aff0db06e0c946a88c345fb07872
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-15 21:05:57 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,7 +41,7 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     user_artist = gets.chomp
 
-    until Artist.all.any?{ |artist| artist.name == user_artist }
+    until valid_artist?(user_artist) # Artist.all.any?{ |artist| artist.name == user_artist }
       puts "Please enter the name of an artist:"
       user_artist = gets.chomp
     end
