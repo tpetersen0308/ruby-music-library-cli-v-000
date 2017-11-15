22e03b60d10e12b4876f6fbf026a40df44bae689
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-15 21:59:46 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,6 +17,9 @@ class MusicLibraryController
     while user_input != 'exit'
       user_input = gets.chomp
     end
+    if user_input = "list songs"
+      self.list_songs
+    end
   end
 
   def list_songs
