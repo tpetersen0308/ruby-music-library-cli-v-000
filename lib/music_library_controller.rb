4fd3e3524a277fb49cf0a9ff23002d504eb38a76
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-15 20:37:04 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -45,6 +45,6 @@ class MusicLibraryController
 
     #Artist.all.detect{ |artist| artist.name == user_artist }.songs.sort_by{ |song| song.name }.each.with_index do |song, index|
     #  puts "#{index + 1}. #{song.name}"
-    end
+    #end
   end
 end
